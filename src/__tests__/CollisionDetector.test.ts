dee335a9189822d89bee6801f2980958826ccadb
Mika Reich <mikareich0@gmail.com>
2021-03-06 12:12:58 +0100

    write tests for collision detector

--- src/__tests__/CollisionDetector.test.ts
+++ src/__tests__/CollisionDetector.test.ts
@@ -15,3 +15,23 @@ test("Object 2 collides with the left side of object 2 with object 1", () => {
   object2.setPosition(200, 100);
   expect(CollisionDetector.hasCollided(object1, object2)).toBe(true);
 });
+
+test("Object 2 collides with the right side of object 2 with object 1", () => {
+  object2.setPosition(0, 100);
+  expect(CollisionDetector.hasCollided(object1, object2)).toBe(true);
+});
+
+test("Object 2 collides with the upper side of object 2 with object 1", () => {
+  object2.setPosition(100, 200);
+  expect(CollisionDetector.hasCollided(object1, object2)).toBe(true);
+});
+
+test("Object 2 collides with the lower side of object 2 with object 1", () => {
+  object2.setPosition(100, 0);
+  expect(CollisionDetector.hasCollided(object1, object2)).toBe(true);
+});
+
+test("Object 2 is in object 1", () => {
+  object2.setPosition(100, 100);
+  expect(CollisionDetector.hasCollided(object1, object2)).toBe(true);
+});
